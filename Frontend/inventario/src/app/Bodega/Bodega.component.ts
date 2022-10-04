9d60a37a7de1d0809b9284924470816708ac4890
pao <ameridad2@miumg.edu.gt>
2022-10-03 23:49:10 -0600

    frontend

---
+++ Frontend/inventario/src/app/Bodega/Bodega.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Bodega',
+  templateUrl: './Bodega.component.html',
+  styleUrls: ['./Bodega.component.css']
+})
+export class BodegaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Frontend/inventario/src/app/Inventario/Inventario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Inventario',
+  templateUrl: './Inventario.component.html',
+  styleUrls: ['./Inventario.component.css']
+})
+export class InventarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
